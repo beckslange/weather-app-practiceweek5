4b22ca81f1b0db3cbcfba682bd5868fffb48e363
beckslange <beckslange96@gmail.com>
2026-07-10 14:30:26 -0600

    added html and styling to app, and added footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,24 @@ function App() {
   return (
     <div className="App">
       <Weather />
+      <footer className="text-center">
+        This project was coded by{" "}
+        <a
+          href="ttps://www.linkedin.com/in/rebecca-lange-804b8b111/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Rebecca Lange
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/beckslange/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -4,8 +4,50 @@ import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div className="Weather mt-5">
-      <h1>Hello World</h1>
+    <div className="Weather">
+      <form className="w-100 justify-content-center">
+        <div className="row mt-4 ms-.5">
+          <div className="col-9 ">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn-primary w-100" />
+          </div>
+        </div>
+      </form>
+
+      <h1>New York</h1>
+      <ul className="list-unstyled">
+        <li>Wednesday 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+
+      <div className="row">
+        <div className="col-6">
+          <div className="d-flex align-items-center">
+            <img
+              src="https://www.gstatic.com/weather/conditions/v1/svg/partly_cloudy_light.svg"
+              alt="Sunny"
+              className="weather-icon me-2"
+            />
+            <span className="temperature">6</span>
+            <span className="unit">°C</span>
+          </div>
+        </div>
+
+        <div className="col-6">
+          <ul className="list-unstyled">
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 72%</li>
+            <li>Wind: 13 km/h</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
